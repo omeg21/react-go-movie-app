81559f88e833fe8588303f091d33e7846e8a66e2
omega <Muhammadomega02@gmail.com>
2023-10-17 15:02:24 +0700

    halfway done with update and add

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,25 +7,48 @@ const Movie = () => {
     let{ id } = useParams();
 
     useEffect(() =>{
-        let myMovie ={
-            id: 1,
-            title:"Zoolander",
-            release_date: "2001-08-27",
-            runtime : 89,
-            mpaa_rating : "R",
-            despription : "Propelled to the top of the fashion world by a photogenic gaze he calls \"Blue Steel,\" dimwitted male model Derek Zoolander (Ben Stiller) thinks he's got a fourth consecutive win as Male Model of the Year in the bag. But, when his rival, Hansel (Owen Wilson), unexpectedly takes the crown, Derek is crushed. He becomes easy prey for fashion designer Jacobim Mugatu (Will Ferrell), who signs Derek to star in his \"Derelicte\" fashion show, then brainwashes him to kill Malaysia's prime minister."
+        const headers = new Headers();
+        headers.append("Content-Type","application/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
         }
 
-        setMovie(myMovie);
+        fetch(`/movies/${id}`,requestOptions)
+            .then((response) => response.json())
+            .then((data) =>{
+                setMovie(data)
+            })
+            .catch(err =>{
+                console.log(err);
+            })
     },[id])
+
+    if (movie.genres){
+        movie.genres = Object.values(movie.genres);
+    }else {
+        movie.genres = [];
+    }
+
+
     return(
         <>
         <div>
             <h2>Movie: {movie.title}</h2>
-            <small><em>{movie.release_date},{movie.runtime} minutes,{movie.mpaa_rating}</em></small>
+            <small><em>{movie.release_date},{movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small> <br/>
+            {movie.genres.map((g) => (
+                <span key={g.genre} className="badge bg-secondary me-2">{g.genre}</span>
+                ))}
             <hr />
 
-            <p>{movie.despription}</p>
+            {movie.image !== "" &&
+                <div className="mb-3">
+                    <img src={`https://image.tmdb.org/t/p/w200/${movie.image}`} alt="poster" ></img>
+                </div>
+            }
+
+            <p>{movie.description}</p>
 
         </div>
         </>
